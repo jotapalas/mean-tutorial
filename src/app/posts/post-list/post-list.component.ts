1557c047ac465351804cf484c781a4c0d36acc55
Juan Palacios Galván <palaciosgalvanj@gmail.com>
2019-04-25 10:17:24 +0200

    Add new Post model and dynamic posts list

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -6,9 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './post-list.component.css' ]
 })
 export class PostListComponent {
-  posts = [
-    {title: 'First post', content: 'First post content' },
-    {title: 'Second post', content: 'Second post content' },
-    {title: 'Third post', content: 'Third post content' }
-  ];
+  @Input() posts: Post[] = [];
 }
