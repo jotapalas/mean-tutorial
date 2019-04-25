990f0885acdfd30ac80101e353b9e1b365d35d65
Juan Palacios Galván <palaciosgalvanj@gmail.com>
2019-04-25 16:22:47 +0200

    Add 'posts' endpoint to api

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware')
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello World!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '12345',
+      title: 'post',
+      content: 'First post on the server'
+    },
+    {
+      id: '54321',
+      title: 'second post',
+      content: 'Second post on the server'
+    },
+  ];
+  res.status(200).json({
+    message: 'Posts fetched!',
+    posts: posts
+  })
 });
 
 module.exports = app;
