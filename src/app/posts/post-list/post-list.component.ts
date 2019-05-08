42f039e91ea801c64cf92e36beb45f28f93abf88
Juan Palacios Galván <palaciosgalvanj@gmail.com>
2019-05-08 12:03:35 +0200

    Add pagination on frontend

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 import { Subscription } from 'rxjs';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-post-list',
@@ -11,7 +12,10 @@ import { Subscription } from 'rxjs';
 })
 export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
+  totalPosts = 10;
   isLoading = false;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
@@ -26,6 +30,10 @@ export class PostListComponent implements OnInit, OnDestroy {
           this.isLoading = false;
         }
       );
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    this.postsPerPage = pageData.pageSize;
   }
 
   onDelete(postId: string){
